41ae7af6fa68c2a599c153dffc68e5deed973c62
vetriselvam <selvamvetri27@gmail.com>
2021-06-28 14:00:53 +0530

    name changes

--- app/Http/Requests/UpdateRegisterFormRequest.php
+++ app/Http/Requests/UpdateRegisterFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use App\RegisterForm;
-class UpdateCategoryRequest extends FormRequest
+class UpdateRegisterFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
